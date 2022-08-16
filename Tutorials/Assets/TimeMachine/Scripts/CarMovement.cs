70aff496c7f244977f48ec197ae5cea2817dbaf3
Артемий <88839467+Ystvlxst@users.noreply.github.com>
2022-08-16 11:46:34 +0500

    wheel collider setting

--- Tutorials/Assets/TimeMachine/Scripts/CarMovement.cs
+++ Tutorials/Assets/TimeMachine/Scripts/CarMovement.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class CarMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
-    [SerializeField] private Animator _animator;
 
     private Rigidbody _rigidbody;
 
@@ -20,11 +17,6 @@ public class CarMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             _rigidbody.velocity = Vector3.forward * _speed;
-            _animator.SetBool("IsMove", true);
-        }
-        else
-        {
-            _animator.SetBool("IsMove", false);
         }
     }
 }
